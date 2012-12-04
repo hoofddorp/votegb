a7ccb7e1a540c99292b59f28d6d735348aaac47d
Rob <rob@rob-VirtualBox.(none)>
2012-12-04 16:03:29 +0000

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Athletes::Application.config.secret_token = '5beec3b08bcbf376415857bf6f1c5910c8a760d1c2c597c4fa8a9eaaca25505e633c820d2ddf452b36eedb35835b1c56db47589e4504931db533e9fb3e816bea'
