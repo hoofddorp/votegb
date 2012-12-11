6dca33e46bfd25aebd0231d74b6dd2ecf47fc11b
Rob <robclark4@gmail.com>
2012-12-11 05:16:23 +0000

    edit review fixed

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -60,7 +60,7 @@ class ReviewsController < ApplicationController
 
     respond_to do |format|
       if @review.update_attributes(params[:review])
-        format.html { redirect_to @review, notice: 'Review was successfully updated.' }
+        format.html { redirect_to @review.athlete, notice: 'Review was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render action: "edit" }
@@ -76,7 +76,7 @@ class ReviewsController < ApplicationController
     @review.destroy
 
     respond_to do |format|
-      format.html { redirect_to reviews_url }
+      format.html { redirect_to @review.athlete }
       format.json { head :no_content }
     end
   end
