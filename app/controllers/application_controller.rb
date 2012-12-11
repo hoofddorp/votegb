a7b66a7eeb8ae8d5da70a0544c7c02cc9585a0f7
Rob <robclark4@gmail.com>
2012-12-11 19:11:51 +0000

    internationalization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  #include TwitterHelper
+  require 'twitter'
     rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_path, :alert => exception.message
+  end
+  
+  before_filter :set_locale
+  def set_locale
+    I18n.locale = params[:lang] if params[:lang].present?
   end
 end
