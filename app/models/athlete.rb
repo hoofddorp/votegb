c73d8562af5b6b4802d791786de44a716592597b
Rob <robclark4@gmail.com>
2012-12-13 03:34:46 +0000

    default image added

--- app/models/athlete.rb
+++ app/models/athlete.rb
@@ -15,8 +15,8 @@ class Athlete < ActiveRecord::Base
   :styles => { :medium => "300x300>", :thumb => "100x100>" },
     :storage => :s3,
     :url => ":s3_domain_url",
-    :default_url => '/avatars/:attachment/missing_:style.jpeg',
-    :path => "/:class/avatars/:id_:basename.:style.:extension",
+    #:default_url => "/avatar/:attachment/missing_:style.jpeg",
+    :path => "/:class/avatar/:id_:basename.:style.:extension",
     :s3_credentials => {
       :bucket            => ENV['S3_BUCKET_NAME'],
       :access_key_id     => ENV['AWS_ACCESS_KEY_ID'],
@@ -24,7 +24,7 @@ class Athlete < ActiveRecord::Base
 else
   has_attached_file :avatar,
    :styles => { :medium => "300x300>", :thumb => "100x100>" },
-   :default_url => '/avatars/:attachment/missing_:style.jpeg'
+   :default_url => "/avatars/medium/missing_:style.jpeg"
    
   end
   validates_attachment_content_type :avatar, :content_type => ['image/jpg', 'image/jpeg', 'image/png', 'image/pjepg', 'image/x-png', 'image/pjpeg']
