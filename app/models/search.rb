a3944c23bbcc57f39b1ccb6aab0ec1e3cdb65730
Rob <robclark4@gmail.com>
2012-12-14 01:29:31 +0000

    search function error fix

--- app/models/search.rb
+++ app/models/search.rb
@@ -11,11 +11,13 @@ private
 
   def find_athletes
     athletes = Athlete.order(:name)
-   
+
     athletes = athletes.where("(name) like ?", "%#{name.upcase}%") if name.upcase.present?
     athletes = athletes.where("(sport) like ?", "%#{sport.upcase}%") if sport.upcase.present?
     athletes = athletes.where("(name) like ?", "%#{name.downcase}%") if name.downcase.present?
     athletes = athletes.where("(sport) like ?", "%#{sport.downcase}%") if sport.downcase.present?
+    ["athletes.name LIKE ?", "%#{name}%"] unless name.blank?
+    ["athletes.sport LIKE ?", "%#{sport}%"] unless sport.blank?
     athletes
   end
 end
